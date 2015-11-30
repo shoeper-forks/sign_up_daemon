f58543de928b16261a5de998896a197877caca70
Jannik Becher <jannik.becher@rwth-aachen.de>
2015-11-30 11:48:52 +0100

    Forget to add the right py script

--- sign_up_daemon.py
+++ sign_up_daemon.py
@@ -1,68 +1,97 @@
 #!/usr/bin/env python
 
 import requests
+import json
 
-def send_sign_up(personal_data):
-    with requests.Session() as s:
-        r = s.get("http://buchung.hsz.rwth-aachen.de/angebote/aktueller_zeitraum/"+personal_data["kurs_name"])
+class Person:
+    def __init__(self, firstname, lastname, email, city, street, matnr, sex):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+        self.city = city
+        self.street = street
+        self.matnr = matnr
+        self.sex = sex
+        self.events = [{}]
 
-        # Get BS_Code from the site
-        bs_code_pos = r.content.find("BS_Code")
-        bs_code = r.content[bs_code_pos+16:bs_code_pos+48] 
-        print bs_code
-        
-        # click on the button "Buchen"
-        data = {
-            "BS_Code":str(bs_code),
-            "BS_Kursid_"+personal_data["kurs_id"]:"buchen"
-        }
+    # event is a dict with course, id, date and time
+    def add_event(self, course_name, course_id, date, time):
+        event = {"course_name": course_name, "course_id": course_id, "date": date, "time": time}
+        self.events.push_back(event)
 
-        header = {
-            "Referer":"http://buchung.hsz.rwth-aachen.de/angebote/aktueller_zeitraum/"+personal_data["kurs_name"]
-        }
+    def send_sign_up(self, event):
+        with requests.Session() as s:
+            r = s.get("http://buchung.hsz.rwth-aachen.de/angebote/aktueller_zeitraum/_"+event["course_name"]+".html")
 
-        r = s.post("https://buchung.hsz.rwth-aachen.de/cgi/anmeldung.fcgi", data = data, headers = header)
+            # Get BS_Code from the site
+            bs_code_pos = r.content.find("BS_Code")
+            bs_code = r.content[bs_code_pos+16:bs_code_pos+48] 
+            print bs_code
+            
+            # click on the button "Buchen"
+            data = {
+                "BS_Code":str(bs_code),
+                "BS_Kursid_"+event["course_id"]:"buchen"
+            }
 
-        # get the fid checksum
-        fid_pos = r.content.find("fid")
-        fid = r.content[fid_pos+12:fid_pos+52]
-        print fid
+            header = {
+                "Referer":"http://buchung.hsz.rwth-aachen.de/angebote/aktueller_zeitraum/_"+event["course_name"]+".html"
+            }
 
-        # choose a date
-        data = {
-            "BS_TERMIN_"+personal_data["termin"]:"buchen",
-            "fid":str(fid)
-        }
+            r = s.post("https://buchung.hsz.rwth-aachen.de/cgi/anmeldung.fcgi", data = data, headers = header)
 
-        header = {
-            "Referer":"https://buchung.hsz.rwth-aachen.de/cgi/anmeldung.fcgi"
-        }
+            # get the fid checksum
+            fid_pos = r.content.find("fid")
+            fid = r.content[fid_pos+12:fid_pos+52]
+            print fid
 
-        r = s.post("https://buchung.hsz.rwth-aachen.de/cgi/anmeldung.fcgi", data = data, headers = header)
+            # choose a date
+            data = {
+                "BS_TERMIN_"+event["date"]:"buchen",
+                "fid":str(fid)
+            }
 
-        # fill the textboxes
-        data = {
-            "Termin":str(personal_data["termin"]),
-            "email":str(personal_data["email"]),
-            "fid":str(fid),
-            "matnr":str(personal_data["matnr"]),
-            "name":str(personal_data["name"]),
-            "ort":str(personal_data["ort"]),
-            "sex":str(personal_data["sex"]),
-            "statusorig":"S-RWTH",
-            "strasse":str(personal_data["strasse"]),
-            "tnbed":"1",
-            "vorname":str(personal_data["vorname"])
-        }
+            header = {
+                "Referer":"https://buchung.hsz.rwth-aachen.de/cgi/anmeldung.fcgi"
+            }
 
-        r = s.post("https://buchung.hsz.rwth-aachen.de/cgi/anmeldung.fcgi", data = data, headers = header)
+            r = s.post("https://buchung.hsz.rwth-aachen.de/cgi/anmeldung.fcgi", data = data, headers = header)
 
-        print r.content
+            # fill the textboxes
+            data = {
+                "Termin":event["data"],
+                "email":self.email,
+                "fid":str(fid),
+                "matnr":self.matnr,
+                "name":self.lastname,
+                "ort":self.city,
+                "sex":self.sex,
+                "statusorig":"S-RWTH",
+                "strasse":self.street,
+                "tnbed":"1",
+                "vorname":self.firstname
+            }
 
-def set_data(kurs_id, kurs_name, termin, email, matnr, name, vorname, ort, sex, strasse):
-    personal_data = {"kurs_id": kurs_id, "kurs_name": kurs_name, "termin": termin, "email": email, "matnr": matnr, "name": name, "ort": ort, "sex": sex, "strasse": strasse, "vorname": vorname}
-    return personal_data
+            r = s.post("https://buchung.hsz.rwth-aachen.de/cgi/anmeldung.fcgi", data = data, headers = header)
+
+            print r.content
+
+    def do_it(self):
+        self.send_sign_up(self, self.events[0])
+
+def create_person(file):
+    with open(file) as data_file:
+        data = json.load(data_file)
+
+    person = Person(data["firstname"], data["lastname"], data["email"], data["city"], data["street"], data["matnr"], data["sex"])
+    for event in data["events"]:
+        person.add_event(event["course"], event["id"], event["date"], event["time"]
+
 
 if(__name__=="__main__"):
-    personal_data = set_data("99582", "_Handball.html", "2015-12-02", "", "334815", "Becher", "Jannik", "52070 Aachen", "M", "Thomashofstr. 31")
-    send_sign_up(personal_data)
+    persons = []
+    with open("config.ini") as ini_file:
+        for file in ini_file.readlines():
+            persons.push_back(create_person(file)
+
+    persons[0].do_it()
