b5fece16fd683dbd42a8f2ccd012c2a640d53256
Jannik Becher <jannik.becher@rwth-aachen.de>
2015-11-30 14:37:29 +0100

    Changed the plan. Now I want to execute the script via cron.

---
+++ send_sign_up.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+import requests
+import sys
+import time
+
+#          0        1           2           3           4          5       6     7     8      9     10
+# argv = [..., course_name, course_id, course_date, firstname, lastname, matnr, sex, email, city, street]
+
+def send_sign_up():
+    with requests.Session() as s:
+        r = s.get("http://buchung.hsz.rwth-aachen.de/angebote/aktueller_zeitraum/_"+sys.argv[1]+".html")
+
+        # Get BS_Code from the site
+        bs_code_pos = r.content.find("BS_Code")
+        bs_code = r.content[bs_code_pos+16:bs_code_pos+48] 
+        
+        # click on the button "Buchen"
+        data = {
+            "BS_Code":str(bs_code),
+            "BS_Kursid_"+sys.argv[2]:"buchen"
+        }
+
+        header = {
+            "Referer":"http://buchung.hsz.rwth-aachen.de/angebote/aktueller_zeitraum/_"+sys.argv[1]+".html"
+        }
+
+        r = s.post("https://buchung.hsz.rwth-aachen.de/cgi/anmeldung.fcgi", data = data, headers = header)
+
+        # get the fid checksum
+        fid_pos = r.content.find("fid")
+        fid = r.content[fid_pos+12:fid_pos+52]
+
+        # choose a date
+        data = {
+            "BS_TERMIN_"+sys.argv[3]:"buchen",
+            "fid":str(fid)
+        }
+
+        header = {
+            "Referer":"https://buchung.hsz.rwth-aachen.de/cgi/anmeldung.fcgi"
+        }
+
+        r = s.post("https://buchung.hsz.rwth-aachen.de/cgi/anmeldung.fcgi", data = data, headers = header)
+
+        # fill the textboxes
+        data = {
+            "Termin":sys.argv[3],
+            "email":sys.argv[8],
+            "fid":str(fid),
+            "matnr":sys.argv[6],
+            "name":sys.argv[5],
+            "ort":sys.argv[9],
+            "sex":sys.argv[7],
+            "statusorig":"S-RWTH",
+            "strasse":sys.argv[10],
+            "tnbed":"1",
+            "vorname":sys.argv[4]
+        }
+
+        r = s.post("https://buchung.hsz.rwth-aachen.de/cgi/anmeldung.fcgi", data = data, headers = header)
+        
+        # binding reserve
+
+        data["Phase"] = "final"
+        data["preis_anz"] = "0,00 EUR"
+        data["pw_newpw_"+fid] = ""
+        r = s.post("https://buchung.hsz.rwth-aachen.de/cgi/anmeldung.fcgi", data = data, headers = header)
+
+        if(r.content.find("This course is unfortunately full!") == -1):
+            print sys.argv[4] + " " +  sys.argv[5] + " is signed up sucessfully!"
+            return False
+        else:
+            print sys.argv[4] + " " +  sys.argv[5] + " is not signed up yet."
+            return True
+
+if(__name__=="__main__"):
+    while(send_sign_up()):
+        time.sleep(30)
