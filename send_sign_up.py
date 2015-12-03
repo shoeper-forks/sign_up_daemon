6c58b76001d2696c985bd7c138c7beff440c9241
Jannik Becher <jannik.becher@rwth-aachen.de>
2015-12-03 22:48:38 +0100

    Es klaptt

--- send_sign_up.py
+++ send_sign_up.py
@@ -6,7 +6,6 @@ import time
 
 #          0        1           2           3           4          5       6     7     8      9     10
 # argv = [..., course_name, course_id, course_date, firstname, lastname, matnr, sex, email, city, street]
-
 def send_sign_up():
     with requests.Session() as s:
         r = s.get("http://buchung.hsz.rwth-aachen.de/angebote/aktueller_zeitraum/_"+sys.argv[1]+".html")
@@ -25,17 +24,17 @@ def send_sign_up():
             "Referer":"http://buchung.hsz.rwth-aachen.de/angebote/aktueller_zeitraum/_"+sys.argv[1]+".html"
         }
 
-
         r = s.post("https://buchung.hsz.rwth-aachen.de/cgi/anmeldung.fcgi", data = data, headers = header)
 
         # get the fid checksum
         fid_pos = r.content.find("fid")
         fid = r.content[fid_pos+12:fid_pos+52]
+        print fid
 
         # choose a date
         data = {
             "BS_Termin_"+sys.argv[3]:"buchen",
-            "fid":str(fid),
+            "fid":str(fid)
         }
 
         header = {
@@ -43,7 +42,9 @@ def send_sign_up():
         }
         
         r = s.post("https://buchung.hsz.rwth-aachen.de/cgi/anmeldung.fcgi", data = data, headers = header)
-
+        
+        time.sleep(10)
+        
         # fill the textboxes
         data = {
             "Termin":sys.argv[3],
@@ -82,13 +83,11 @@ def send_sign_up():
             "preis_anz":"0,00 EUR",
             "pw_newpw_"+fid:""
         }
+
         r = s.post("https://buchung.hsz.rwth-aachen.de/cgi/anmeldung.fcgi", data = data, headers = header)
 
         print r.text
 
-
-        print r.status_code
-        print r.history
         if(r.status_code == requests.codes.found):
             print sys.argv[4] + " " +  sys.argv[5] + " is signed up sucessfully!"
             return False
